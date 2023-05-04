db198d32e7e6e0ba661a8e1f7978ae0637192ac4
Ryan Oliveira <jryancoelho79@hotmail.com>
2023-05-04 00:34:08 -0300

    build: configuring env and paths

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,33 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [require.resolve("expo-router/babel")],
+    plugins: [
+      [
+        'module:react-native-dotenv',
+        {
+          envName: 'APP_ENV',
+          moduleName: '@env',
+          path: '.env',
+        },
+      ],
+      require.resolve("expo-router/babel"),
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@/components': './src/components',
+            '@/assets': './src/assets',
+            '@/theme': './src/theme',
+            '@/app': './src/app',
+            '@/lib': './src/lib',
+            '@/services': './src/services',
+            '@/utils': './src/utils',
+            '@/contexts': './src/contexts',
+            '@/hooks': './src/hooks',
+          },
+        },
+      ],
+    ],
   };
 };
