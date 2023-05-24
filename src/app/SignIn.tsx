336a9be62e117be15b05d774c3b487d9b6605520
Stênio do Carmo <steniobotelhodocarmo@gmail.com>
2023-05-24 14:29:35 -0300

    feat: input form validation

--- src/app/SignIn.tsx
+++ src/app/SignIn.tsx
@@ -1,32 +1,49 @@
 import { Text, VStack, Center, HStack } from "native-base";
+import { z, ZodType } from "zod";
+import { useForm, Controller } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 import auth from "@react-native-firebase/auth";
-import SignIcon from "@/assets/signicon.svg"
-import SignFrame from "@/assets/signframe.svg"
-import SignFrame2 from "@/assets/signframe2.svg"
+
+import SignIcon from "@/assets/signicon.svg";
+import SignFrame from "@/assets/signframe.svg";
+import SignFrame2 from "@/assets/signframe2.svg";
 
 import { Input } from "@/components/Input";
 import { Button } from "@/components/SignButton";
 
+type FormData = {
+  email: string;
+  password: string;
+};
+
+const schema: ZodType<FormData> = z.object({
+  email: z.string().nonempty("Digite o email").email("Email inválido"),
+  password: z.string().min(6, "A senha precisa de no mínimo 6 caracteres"),
+});
+
 export function SignIn() {
-  function handleSignIn() {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
+
+  function handleSignIn({ email, password }: FormData) {
+    console.log({ email, password });
     auth()
-      .signInWithEmailAndPassword("teste@teste.com", "123456")
+      .signInWithEmailAndPassword(email, password)
       .then((user) => console.log(user))
       .catch((error) => console.log(error));
   }
 
   return (
-    <VStack 
-      flex={1}
-      bg="purple.90"
-    >
-
+    <VStack flex={1} bg="purple.90">
       <Center>
         <HStack>
-          <Text 
+          <Text
             mt={12}
-            fontSize="xl_3" 
+            fontSize="xl_3"
             fontFamily="light"
             color="black.5"
             left={32}
@@ -39,37 +56,48 @@ export function SignIn() {
           <SignFrame />
         </HStack>
       </Center>
-      
+
       <Center bottom={48}>
         <SignIcon />
-        
-        <Input 
-          placeholder="Email"
+
+        <Controller
+          control={control}
+          name="email"
+          render={({ field: { onChange, value } }) => (
+            <Input
+              placeholder="E-mail"
+              keyboardType="email-address"
+              autoCapitalize="none"
+              onChangeText={onChange}
+              value={value}
+              errorMessage={errors.email?.message}
+            />
+          )}
         />
 
-        <Input 
-          placeholder="Senha"
-          mb={3}
+        <Controller
+          control={control}
+          name="password"
+          render={({ field: { onChange, value } }) => (
+            <Input
+              placeholder="Senha"
+              secureTextEntry
+              onChangeText={onChange}
+              value={value}
+              errorMessage={errors.password?.message}
+            />
+          )}
         />
 
-        <Text 
-          fontSize="sm" 
-          color="black.5" 
-          mb={8}
-          right={24}
-        >
+        <Text fontSize="sm" color="black.5" mb={8} right={24}>
           Esqueceu a senha?
         </Text>
 
-        <Button 
-          onPress={handleSignIn} 
-          title="Cadastrar"
-        />
+        <Button onPress={handleSubmit(handleSignIn)} title="Cadastrar" />
         <HStack right={32} bottom={10}>
           <SignFrame2 />
         </HStack>
       </Center>
-      
     </VStack>
   );
 }
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,23 +1,37 @@
-import { Input as NativeBaseInput, IInputProps } from "native-base";
+import {
+  Input as NativeBaseInput,
+  IInputProps,
+  FormControl,
+} from "native-base";
 
-export function Input({...rest}: IInputProps){
-  return(
-    <NativeBaseInput 
-      h={14}
-      w={80}
-      px={4}
-      borderWidth={1}
-      borderRadius={6}
-      fontSize="md"
-      fontFamily="regular"
-      color="black.5"
-      mb={8}
-      placeholderTextColor="black.5"
-      _focus={{
-        bg: "transparent",
-        borderColor: "black.5"
-      }}
-      {...rest}
-    />
-  )
+type Props = IInputProps & {
+  errorMessage?: string 
+};
+
+export function Input({ errorMessage, isInvalid, ...rest }: Props) {
+  const invalid = !!errorMessage || isInvalid;
+
+  return (
+    <FormControl alignItems="center" isInvalid={!!errorMessage} mb={8}>
+      <NativeBaseInput
+        h={14}
+        w={80}
+        px={4}
+        borderWidth={1}
+        borderRadius={6}
+        fontSize="md"
+        fontFamily="regular"
+        color="black.5"
+        isInvalid={invalid}
+        placeholderTextColor="black.5"
+        _focus={{
+          bg: "transparent",
+          borderColor: "black.5",
+        }}
+        {...rest}
+      />
+
+      {errorMessage && (<FormControl.ErrorMessage>{errorMessage}</FormControl.ErrorMessage>)}
+    </FormControl>
+  );
 }
