3b2aaea5f54dc508b7283860592194607037c545
Stênio do Carmo <steniobotelhodocarmo@gmail.com>
2023-05-29 22:28:31 -0300

    fix: fonts loading when app initializes

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,24 @@
 import { StatusBar } from "react-native";
 
+import {
+  Comfortaa_300Light,
+  Comfortaa_400Regular,
+  Comfortaa_500Medium,
+  Comfortaa_700Bold,
+  useFonts,
+} from "@expo-google-fonts/comfortaa";
 import { NativeBaseProvider } from "native-base";
 
 import { SignIn } from "@/app/SignIn";
 import { THEME } from "@/theme/index";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Comfortaa_300Light,
+    Comfortaa_400Regular,
+    Comfortaa_500Medium,
+    Comfortaa_700Bold,
+  });
   return (
     <NativeBaseProvider theme={THEME}>
       <StatusBar
@@ -13,6 +26,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
+      {fontsLoaded}
       <SignIn />
     </NativeBaseProvider>
   );
